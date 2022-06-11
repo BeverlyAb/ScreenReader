809d0f9b6a9d7000c8fde76e98c4c31cbb911e52
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2022-06-11 03:17:05 -0700

    init commit

---
+++ screen_reader.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pyautogui
+import time
+import pytesseract
+from PIL import ImageOps
+
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+OFFSET_LEFT, OFFSET_WIDTH, SAMPLE_RATE_IN_SEC = 110,50, 60*10
+
+# get image location
+loc = pyautogui.locateOnScreen('waitingServer.JPG', confidence=0.7)
+if loc != None:
+    img = pyautogui.screenshot(region = (loc.left + OFFSET_LEFT,loc.top, loc.width+ OFFSET_WIDTH,loc.height))
+    # display number, if img looks good move on to sampling
+    plt.imshow(img)
+    plt.show()
+
+    # get queue number every sample
+    num_queue = float(pytesseract.image_to_string(ImageOps.grayscale(img)))
+    print('Queue num:' , num_queue)
+    while(num_queue > 100):
+        time.sleep(SAMPLE_RATE_IN_SEC)
+        num_queue = float(pytesseract.image_to_string(ImageOps.grayscale(img)))
+        print('Queue num:' , num_queue)
+        if loc != None:
+            img = pyautogui.screenshot(region = (loc.left + OFFSET_LEFT,loc.top, loc.width+ OFFSET_WIDTH,loc.height))
+else:
+    print('Waiting for Server text box not detected')
+
